12be35243376daebeb9d3b04522ef5b8e999e2ee
Oskarfug <oskarfuglem@gmail.com>
2025-09-12 13:37:07 +0200

    Wordle: created readFile method and tests for the method

--- src/main/java/wordle/TextFileReader.java
+++ src/main/java/wordle/TextFileReader.java
@@ -1,4 +1,16 @@
 package wordle;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class TextFileReader {
+    public static List<String> readFile(Path filePath) {
+        try {
+            return Files.readAllLines(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/wordle/TextFileReaderTest.java
+++ src/test/java/wordle/TextFileReaderTest.java
@@ -1,4 +1,58 @@
 package wordle;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TextFileReaderTest {
+    @Test
+    void canReadAllWordsFromTestFile(@TempDir Path tempDir) throws IOException {
+        // Arrange
+        Path tempFile = tempDir.resolve("testFile.txt");
+        List<String> linesToWrite = List.of("WATER", "STORY", "TRAIN");
+
+        Files.write(tempFile, linesToWrite);
+
+        // Act
+        List<String> words = TextFileReader.readFile(tempFile);
+
+
+        // Assert
+        assertEquals(3, words.size());
+        assertEquals("WATER", words.get(0));
+        assertEquals("STORY", words.get(1));
+        assertEquals("TRAIN", words.get(2));
+    }
+
+    @Test
+    void handlesEmptyFile(@TempDir Path tempDir) throws IOException {
+        // Arrange
+        Path emptyFile = tempDir.resolve("emptyFile.txt");
+        Files.createFile(emptyFile);
+
+        // Act
+        List<String> words = TextFileReader.readFile(emptyFile);
+
+        // Assert
+        assertEquals(0, words.size());
+    }
+
+    @Test
+    void handlesNonExistingFile() {
+        // Arrange
+        Path nonExistingPath = Path.of("nonExistingFile.txt");
+
+        // Assert
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            TextFileReader.readFile(nonExistingPath);
+        });
+
+        assertInstanceOf(IOException.class, exception.getCause());
+    }
 }
